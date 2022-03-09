83b02ad069ff28f8cf76b63da90e61b52835c45a
GotemBarbosa <gotembarbosagd@gmail.com>
2022-03-09 13:49:51 -0300

    #5 GraphQl fix

--- src/route/ProductListing/ProductListing.js
+++ src/route/ProductListing/ProductListing.js
@@ -1,19 +1,84 @@
-import React from 'react';
+import React from "react";
+import { gql } from "@apollo/client";
+import { graphql } from "react-apollo";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import './ProductListing.style.css'
+import "./ProductListing.style.css";
 
-class ProductListing extends React.Component{
-    render(){
-        return(
-            <div className='ProductListing'>
-                <div className='ProductListing-CategoryTitle'>{this.props.categoryName}</div>
-            </div>
-        )
+const getNewProducts = gql`
+  query ($type: String!) {
+    category(input: { title: $type }) {
+      name
+      products {
+        id
+        name
+        inStock
+        gallery
+        description
+        category
+        attributes {
+          id
+          name
+          type
+          items {
+            displayValue
+            value
+            id
+          }
+        }
+        prices {
+          currency {
+            label
+            symbol
+          }
+          amount
+        }
+        brand
+      }
     }
+  }
+`;
+
+class ProductListing extends React.Component {
+
+  displayProducts() {
+    //const {products} = this.props.data.category;
+    const data = this.props.data
+    if(data.loading){
+        return(<div>...</div>)
+    }
+      return data.category.products.map((product, key) => (
+        <div key={key}>
+          Nome: {product.name} - {product.id}
+        </div>
+      ));
+  }
+
+  render() {
+      console.log(this.props)
+    return (
+      <div className="ProductListing">
+        <div className="ProductListing-CategoryTitle">
+          {this.props.categoryName}
+        </div>
+
+        <div className="ProductListing-Products">{this.displayProducts()}</div>
+      </div>
+    );
+  }
 }
 
-export default connect(state=>({
-    categoryName: state.category.activeCategoryName
-}))(ProductListing)
+export default connect((state) => ({
+  categoryName: state.category.activeCategoryName,
+}))(
+  graphql(getNewProducts, {
+    options: (props) => {
+      return {
+        variables: {
+          type: props.categoryName,
+        },
+      };
+    },
+  })(ProductListing)
+);
